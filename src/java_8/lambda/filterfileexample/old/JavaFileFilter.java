2c81596076d9c4c14533340f75092aeda8c3e8ab
Filipe Parente <lfmparente@gmail.com>
2020-09-12 08:23:42 +0200

    1.4 - The fileFilter example

---
+++ src/java_8/lambda/filterfileexample/old/JavaFileFilter.java
@@ -0,0 +1,13 @@
+package java_8.lambda.filterfileexample.old;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class JavaFileFilter implements FileFilter{
+
+	@Override
+	public boolean accept(File file) {
+		return file.getName().endsWith(".java");
+	}
+
+}
---
+++ src/java_8/lambda/filterfileexample/old/TestJavaFileFilter.java
@@ -0,0 +1,45 @@
+package java_8.lambda.filterfileexample.old;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class TestJavaFileFilter {
+
+  /**
+   * anonymous class
+   */
+  static FileFilter fileFilter = new FileFilter() {
+
+    @Override
+    public boolean accept(File file) {
+      return file.getName().endsWith(".java");
+    }
+
+  };
+
+
+  public static void main(String[] args) {
+    listFIles();
+    listFIlesV2();
+  }
+
+  private static void listFIles() {
+
+    JavaFileFilter fileFilter = new JavaFileFilter();
+    File dir = new File("C:\\tmp");
+    File[] javaFiles = dir.listFiles(fileFilter);
+
+    System.out.println(javaFiles.length);
+  }
+
+
+  private static void listFIlesV2() {
+
+    File dir = new File("C:\\tmp");
+    File[] javaFiles = dir.listFiles(fileFilter);
+
+    System.out.println(javaFiles.length);
+  }
+
+
+}
