57386b04ea099734300c6875d0c74384ed59f7fe
Filipe Parente <lfmparente@gmail.com>
2020-09-19 14:28:08 +0200

    1.17 (live coding) chain comsumers

---
+++ src/section1/java_8/live/coding/ChainConsumers.java
@@ -0,0 +1,24 @@
+package section1.java_8.live.coding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ChainConsumers {
+
+  public static void main(String... args) {
+
+    List<String> strings = Arrays.asList("one", "two", "three", "four", "five");
+
+    List<String> result = new ArrayList<>();
+
+    Consumer<String> c1 = System.out::println;
+    Consumer<String> c2 = result::add;
+
+    strings.forEach(c1.andThen(c2));
+    result.forEach(System.out::println);
+
+  }
+
+}
